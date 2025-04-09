25d997ada8a91eadfc206ca5c50c8bcf304de865
Lukas Pertl <ljfp2@cam.ac.uk>
2025-04-09 14:38:36 +0200

    Graphs

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Runner import run_multiple_experiments
 from ExperimentalPipeline1 import main
 
 if __name__ == '__main__':
-    specific_rows = [24]
+    specific_rows = [9]
     Mode = "simple"
     main(specific_rows, Mode)
 
