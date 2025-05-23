2d289d839b5371c4882fd9b607d419ce075cd0f6
Lukas Pertl <ljfp2@cam.ac.uk>
2025-05-23 17:34:40 +0100

    Leaky plotting

--- ExperimentalPipeline1.py
+++ ExperimentalPipeline1.py
@@ -26,7 +26,7 @@ def run_single_experiment(experiment_config):
     print(f"\nRunning experiment: mode={experiment_config['mode']} | model_type={experiment_config['model_type']}")
     
     # Run the experiments.
-    results, all_model_params, all_average_embeddings, empty_graph_stats, avg_predictions = run_multiple_experiments(experiment_config, num_experiments=1)
+    results, all_model_params, all_average_embeddings, empty_graph_stats, avg_predictions = run_multiple_experiments(experiment_config, num_experiments=15)
     print(f"Average predictions: {avg_predictions}")
     # Condense empty graph stats
     empty_graph_stats = mean_std_global(empty_graph_stats)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ExperimentalPipeline1 import main
 
 # Specific rows correspond to configurations defined in the CSV files (See ExperimentList folder).
 if __name__ == '__main__':
-    specific_rows = [2]
-    Mode = "tox21"
+    specific_rows = [9]
+    Mode = "simple"
     main(specific_rows, Mode)
 
